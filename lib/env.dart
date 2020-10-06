e8bae59f45b6ccd9e649493cb1341749555be818
Katekko <joaovixgon@gmail.com>
2020-10-06 14:54:38 -0300

    Changing env

--- lib/env.dart
+++ lib/env.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 class Env {
-  static final host = Platform.environment['HOST'] ?? '192.168.0.2';
+  static final host = '0.0.0.0' ?? '192.168.0.2';
   static final port = Platform.environment['PORT'] ?? '5001';
-  static final jwtKey = Platform.environment['jwt-key'] ?? 'S3CR3T';
+  static final jwtKey = Platform.environment['JWT_KEY'] ?? 'S3CR3T';
 }
