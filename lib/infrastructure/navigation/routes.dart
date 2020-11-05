89975305ae8d548bbdfee8c8ab52bdc604bd4496
Katekko <joaovixgon@gmail.com>
2020-11-05 10:14:38 -0300

    Improving the routes

--- lib/infrastructure/navigation/routes.dart
+++ lib/infrastructure/navigation/routes.dart
@@ -1,10 +1,15 @@
+class Controllers {
+  static const AUTH = '/api/auth';
+  static const USER = '/api/user';
+}
+
 class Routes {
   static Future<String> get initialRoute async {
     return HOME;
   }
 
   static const HOME = '/';
-  static const LOGIN = '/api/auth/login';
-  static const RECOVER_PASSWORD = '/api/auth/recover-password';
-  static const GET_USER_INFO = '/api/user';
+  static const LOGIN = '${Controllers.AUTH}/login';
+  static const RECOVER_PASSWORD = '${Controllers.AUTH}/recover-password';
+  static const GET_USER_INFO = '${Controllers.USER}';
 }
