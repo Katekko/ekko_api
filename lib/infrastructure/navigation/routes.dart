f7fd923e636df1a6b44fb05d6c29f1a5eab2d909
Katekko <joaovixgon@gmail.com>
2020-11-05 12:50:34 -0300

    Changing routes

--- lib/infrastructure/navigation/routes.dart
+++ lib/infrastructure/navigation/routes.dart
@@ -1,15 +1,10 @@
-class Controllers {
-  static const AUTH = '/api/auth';
-  static const USER = '/api/user';
-}
-
 class Routes {
   static Future<String> get initialRoute async {
     return HOME;
   }
 
   static const HOME = '/';
-  static const LOGIN = '${Controllers.AUTH}/login';
-  static const RECOVER_PASSWORD = '${Controllers.AUTH}/recover-password';
-  static const GET_USER_INFO = '${Controllers.USER}';
+  static const LOGIN = '/api/auth/login';
+  static const RECOVER_PASSWORD = '/api/auth/recover-password';
+  static const GET_USER_INFO = '/api/user';
 }
